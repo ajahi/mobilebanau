fb33f242c47f45d3b59436b3ac97fa939bb45dea
ajahi <himaliamit1@gmail.com>
2021-07-10 12:10:59 +0545

    first commit

---
+++ app/Mechanic.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Mechanic extends Model
+{
+    protected $fillable=['name','experties','dob','citizenship_details'];
+    
+}
